be56d910c40e39de212fe85035445b75e043e4c3
alexcvoss <avoss14@unl.edu>
2025-04-05 14:47:44 -0500

    change Tkinter to tkinter

--- src/control_topic/control_topic/gui.py
+++ src/control_topic/control_topic/gui.py
@@ -4,6 +4,6 @@ from std_msgs.msg import Int16, Int16MultiArray
 import os
 try:
     import tkinter as tk
-except ImportError:  # Python 2.x present
-    import Tkinter as tk
+except ImportError: 
+    import tkinter as tk
 
