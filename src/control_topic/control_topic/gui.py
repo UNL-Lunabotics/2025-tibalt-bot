792533ed72c01180420924c61657839c60f47efe
alexcvoss <avoss14@unl.edu>
2025-04-05 14:41:25 -0500

    gui stuff

--- src/control_topic/control_topic/gui.py
+++ src/control_topic/control_topic/gui.py
@@ -1,1 +1,9 @@
 # TODO: update using code from last year
+import rclpy
+from std_msgs.msg import Int16, Int16MultiArray
+import os
+try:
+    import tkinter as tk
+except ImportError:  # Python 2.x present
+    import Tkinter as tk
+
