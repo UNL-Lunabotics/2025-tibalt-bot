fd5743d3867f927519c877ac197a54d784c940ed
MorganCude <143128008+MorganCude@users.noreply.github.com>
2025-04-19 16:16:39 -0500

    POL time

--- arduino/terrenceArduino/terrenceArduino.ino
+++ arduino/terrenceArduino/terrenceArduino.ino
@@ -37,34 +37,6 @@ void setup() {
       Serial.println(input);
 
       if (input == "start") {
-        dt[0].writeMicroseconds(dtForward);
-        dt[1].writeMicroseconds(dtForward);
-        dt[2].writeMicroseconds(dtBackward);
-        dt[3].writeMicroseconds(dtBackward);
-        delay(10000);
-        dt[0].writeMicroseconds(dtBackward);
-        dt[1].writeMicroseconds(dtBackward);
-        dt[2].writeMicroseconds(dtForward);
-        dt[3].writeMicroseconds(dtForward);
-        delay(10000);
-        dt[0].writeMicroseconds(dtStop);
-        dt[1].writeMicroseconds(dtStop);
-        dt[2].writeMicroseconds(dtStop);
-        dt[3].writeMicroseconds(dtStop);
-        delay(10000);
-        rcHopLift.ForwardBackwardM2(ROBOCLAW_ADDRESS, 0);
-        delay(20000);
-        //TODO: open latch
-        // delay(1000);
-        rcHopLift.ForwardBackwardM2(ROBOCLAW_ADDRESS, 127);
-        delay(20000);
-
-        dt[0].writeMicroseconds(dtStop);
-        dt[1].writeMicroseconds(dtStop);
-        dt[2].writeMicroseconds(dtStop);
-        dt[3].writeMicroseconds(dtStop);
-        rcHopLift.ForwardBackwardM2(ROBOCLAW_ADDRESS, 64);
-
         runLoop = true;
         Serial.println("Starting autonomous loop.");
         delay(100); // Optional short delay before starting
@@ -75,5 +47,25 @@ void setup() {
 }
 
 void loop() {
-
+  dt[0].writeMicroseconds(dtBackward);
+  dt[1].writeMicroseconds(dtBackward);
+  dt[2].writeMicroseconds(dtForward);
+  dt[3].writeMicroseconds(dtForward);
+  delay(10000);
+  dt[0].writeMicroseconds(dtForward);
+  dt[1].writeMicroseconds(dtForward);
+  dt[2].writeMicroseconds(dtBackward);
+  dt[3].writeMicroseconds(dtBackward);
+  delay(10000);
+  dt[0].writeMicroseconds(dtStop);
+  dt[1].writeMicroseconds(dtStop);
+  dt[2].writeMicroseconds(dtStop);
+  dt[3].writeMicroseconds(dtStop);
+  delay(1000);
+  rcHopLift.ForwardBackwardM2(ROBOCLAW_ADDRESS, 0);
+  delay(30000);
+  //TODO: open latch
+  // delay(1000);
+  rcHopLift.ForwardBackwardM2(ROBOCLAW_ADDRESS, 127);
+  delay(30000);
 }
