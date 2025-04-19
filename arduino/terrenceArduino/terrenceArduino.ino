360a914db29227c41f005c4d7815e65a776f65e1
MorganCude <143128008+MorganCude@users.noreply.github.com>
2025-04-19 12:23:13 -0500

    Testing stuff

--- arduino/terrenceArduino/terrenceArduino.ino
+++ arduino/terrenceArduino/terrenceArduino.ino
@@ -2,21 +2,59 @@
 
 #define ROBOCLAW_ADDRESS 0x80
 
-SoftwareSerial serialTest(18, 19);  // TX=18, RX=19
-RoboClaw roboclaw(&serialTest, 10000);
+//Commands:
+// screen /dev/ttyACM0 115200
+// start
+
+SoftwareSerial serialDt(18, 19);  //TODO
+RoboClaw rcDt(&serialDt, 10000);
+
+SoftwareSerial serialHopLift(20, 21); //TODO
+RoboClaw rcHopLift(&serialHopLift, 10000);
+
+//TODO: hopper servo latch
 
 bool runLoop = false;
 
 void setup() {
-  serialTest.begin(9600);
-  roboclaw.begin(38400);
-  Serial.begin(115200);  // Serial from Jetson or PC
+  Serial.begin(9600);
+
+  rcDt.begin(38400);
+  rcDt.begin(38400);
+
+
+  //TODO: just here for testing things idk
+  // rcDt.ForwardBackwardM1(ROBOCLAW_ADDRESS, 70);
+  // rcDt.ForwardBackwardM2(ROBOCLAW_ADDRESS, 48);
+  // delay(1000);
+  // rcDt.ForwardBackwardM1(ROBOCLAW_ADDRESS, 64);
+  // rcDt.ForwardBackwardM2(ROBOCLAW_ADDRESS, 64);
+  // delay(1000);
+  // rcHopLift.ForwardBackwardM1(ROBOCLAW_ADDRESS, 100);
+  // delay(1000);
+  // //TODO: open latch
+  // delay(1000);
+  // rcHopLift.ForwardBackwardM1(ROBOCLAW_ADDRESS, 28);
+  // delay(1000);
+  // //TODO: close latch
+  // delay(1000);
+  // rcHopLift.ForwardBackwardM1(ROBOCLAW_ADDRESS, 100);
+  // delay(1000);
+  // //TODO: open latch
+  // delay(1000);
+  // rcHopLift.ForwardBackwardM1(ROBOCLAW_ADDRESS, 28);
+  // delay(1000);
+  // //TODO: close latch
+  // delay(1000);
+  // rcHopLift.ForwardBackwardM1(ROBOCLAW_ADDRESS, 64);
+
 
   // Wait until we get a "start" signal over Serial
   while (!runLoop) {
     if (Serial.available()) {
       String input = Serial.readStringUntil('\n');
       input.trim();
+      Serial.println(input);
 
       if (input == "start") {
         runLoop = true;
@@ -30,7 +68,8 @@ void setup() {
 
 void loop() {
   if (runLoop) {
-    roboclaw.ForwardBackwardM2(ROBOCLAW_ADDRESS, 127);  // Full forward
-    delay(100); // Adjust delay to control loop rate if needed
+    rcDt.ForwardBackwardM1(ROBOCLAW_ADDRESS, 64);
+    rcDt.ForwardBackwardM2(ROBOCLAW_ADDRESS, 64);
+    rcHopLift.ForwardBackwardM1(ROBOCLAW_ADDRESS, 64);
   }
 }
