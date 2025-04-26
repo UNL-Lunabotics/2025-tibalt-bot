082f71073998929c95403460e0258072759420c8
Ella Moody <moodyellam@gmail.com>
2025-04-26 18:55:52 -0500

    implemented excavation logic

--- src/control_topic/control_topic/tibalt_control.py
+++ src/control_topic/control_topic/tibalt_control.py
@@ -12,6 +12,7 @@ from std_msgs.msg import Int16MultiArray
 
 import time
 
+'''CONSTANT DECLARATION'''
 # Motor speed constants to help make the code more readable
 # They range from 0-127, with 0 as full power backward, 64 as stop, and 127 as full power forward
 MOTOR_FORWARDS = 127
@@ -30,12 +31,24 @@ HOPPER_EXTEND_AND_RETRACT_PERIOD = 10 # This is how long it should take to exten
 HOPPER_PAUSE_PERIOD = 5 # This is how long the hopper should pause when fully extended before retracting
                         # In seconds
 
+EXCAVATION_MOTOR_INCREMENT = 10
+EXCAVATION_RETRACT_PERIOD = 10  # period of time (in seconds) for the linear atuator to retract to 0cm
+
+'''CONTROL SCHEME'''
+
 
 class HOPPER_STATE(enumerate):
   """Enum to keep track of the current hopper state."""
   RESTING = 0
   EXTENDING = 1
   DUMPING = 2
+  RETRACTING = 3
+
+class EXCAV_STATE(enumerate):
+  """Enum to keep track of the excavation states."""
+  RESTING = 0
+  EXTENDING = 1
+  DIGGING = 2
   RETRACTING = 3
 
 class Tibalt(Node):
@@ -51,6 +64,12 @@ class Tibalt(Node):
     self.hopper_vibration_motor = MOTOR_STOP
     self.hopper_actuator_motor = MOTOR_STOP
     self.hopper_latch_servo = SERVO_FULL_CLOSE
+
+    # Excavation motor variables
+    self.excavation_state = EXCAV_STATE.RESTING
+    self.excavation_timer = 0
+    self.excavation_spin_motor = MOTOR_STOP
+    self.excavation_actuator_motor = MOTOR_STOP
 
     # This creates a Rate object that we will use to sleep the system at the specified frequency (in Hz)
     # We need this to make sure the node's don't publish data too quickly
@@ -87,7 +106,83 @@ class Tibalt(Node):
     DtLeft: int = 64 + forward + turn
     DtRight: int = 64 + forward - turn
     
-    # TODO: excavation logic (1 motor for turning scoops, 1 linear actuator to push into ground and retract)
+
+
+    ########## EXCAVATION LOGIC ##########
+
+    def incrementMotor(motorSpeed, max):
+      if (motorSpeed + EXCAVATION_MOTOR_INCREMENT > max):
+          return max
+      else:
+          return motorSpeed + EXCAVATION_MOTOR_INCREMENT
+    
+    def decrementMotor(motorSpeed, min):
+        if (motorSpeed - EXCAVATION_MOTOR_INCREMENT < min):
+          return min
+        else:
+          return motorSpeed - EXCAVATION_MOTOR_INCREMENT
+
+    
+    # In the resting state, both motors are not moving. Check if the extend
+    # button is pressed
+    if self.excavation_state == EXCAV_STATE.RESTING:
+      self.excavation_spin_motor = MOTOR_STOP
+      self.excavation_actuator_motor = MOTOR_STOP
+
+      # move on to extending
+      if joystick.buttons[extend_button] == 1:
+        self.excavation_state = EXCAV_STATE.EXTENDING
+
+    # In the extending state, the actuator is extending while the excavator is spinning.
+    # The motors are in this state as long as the extending button remains pressed.
+    # check if the button is released
+    if self.excavation_state == EXCAV_STATE.EXTENDING:
+
+      # Increment values while under 127
+      self.excavation_spin_motor = incrementMotor(self.excavation_spin_motor, MOTOR_FORWARDS)
+      self.excavation_actuator_motor = incrementMotor(self.excavation_actuator_motor, MOTOR_FORWARDS)
+    
+      # if the extending button is released move to digging
+      if joystick.buttons[extend_button] == 0:
+        self.excavation_state = EXCAV_STATE.DIGGING
+    
+    # In the digging state, the actuator is still while the excavator is still
+    # spinning. Check if the extend button or the retract button is pressed.
+    # Move to the extend or retract state accordingly
+    if self.excavation_state == EXCAV_STATE.DIGGING:
+
+      #Increment values while under 127(MOTOR_FORWARDS)
+      self.excavation_spin_motor = incrementMotor(self.excavation_spin_motor, MOTOR_FORWARDS)
+      self.excavation_actuator_motor = decrementMotor(self.excavation_actuator_motor, MOTOR_STOP)
+
+      # can return to the extending state
+      if joystick.buttons[extend_button] == 1:
+        self.excavation_state = EXCAV_STATE.EXTENDING
+
+      elif joystick.buttons[retract_button] == 1:
+        # records the time the retract button was pressed
+        self.excavation_timer = time.time()
+        self.excavation_state = EXCAV_STATE.RETRACTING
+
+    # In the retracting state, the excavator is still spinning while the
+    # actuator is retracting. Stays in the retracting state for a set
+    # amount of time.
+    if self.excavation_state == EXCAV_STATE.RETRACTING:
+      # retract state motor status
+      self.excavation_spin_motor = incrementMotor(self.excavation_spin_motor, MOTOR_FORWARDS)
+
+      # incase the actuator is still moving forward
+      if (self.excavation_actuator_motor > MOTOR_STOP):
+        #sets it to the increment value so that it will be at MOTOR_STOP once decrementMotor() is called
+        self.excavation_actuator_motor = EXCAVATION_MOTOR_INCREMENT
+
+      self.excavation_actuator_motor = decrementMotor(self.excavation_actuator_motor, MOTOR_BACKWARDS)
+
+      # once the retract period has expired, go to resting
+      if time.time() - self.excavation_timer > EXCAVATION_RETRACT_PERIOD:
+        self.excavation_state = EXCAV_STATE.RESTING
+
+
 
     ########## HOPPER LOGIC ##########
 
@@ -156,6 +251,9 @@ class Tibalt(Node):
 
         self.hopper_state = HOPPER_STATE.RESTING
     
+
+
+    ########## PUBLISH SPEEDS ##########
     # TODO: publish all motor speeds here (need to decide on order based on total amount of motors for each system)
     motor_speeds = Int16MultiArray()
     # [dtLeft, dtRight, exTurn, exActuator, hopperActuator, hopperHatch, hopperVibe] (variable names can change but this is the order for the motors)
