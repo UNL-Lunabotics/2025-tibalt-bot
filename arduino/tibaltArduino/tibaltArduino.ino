30f0548364a7db55bc63fb081bf0de666c0be05e
Caleb Hans <129148235+caleb-hansolo@users.noreply.github.com>
2025-05-10 17:08:43 -0500

    refactored arduino code to match current wiring

--- arduino/tibaltArduino/tibaltArduino.ino
+++ arduino/tibaltArduino/tibaltArduino.ino
@@ -8,58 +8,89 @@
 #include <Servo.h>
 
 #define ROBOCLAW_ADDRESS 0x80
+#define ROBOCLAW_BAUD 38400
 
-RoboClaw roboclaw1(&Serial1, 10000); // M1: excavation drive M2: hopper linear actuator
-RoboClaw roboclaw2(&Serial2, 10000);
+// RoboClaw definitions
+RoboClaw roboclaw1(&Serial1, 10000); // M1 = Hopper Linear, M2 = Excavation Motor | Teensy Tx = 1, Rx = 0
+RoboClaw roboclaw2(&Serial2, 10000); // M1 = Excavation Linear                    | Teensy Tx = 8, Rx = 7
 
-Servo dtRight[2];
-Servo dtLeft[2];
+// Servo definitions
+Servo dtRight;
+Servo dtLeft;
+Servo hopperServo;
 
-Servo hopperLatch;
+// Pin Assignments
+int hopperServoPin = 9;
+int dtRightPin = 4;
+int dtLeftPin = 5;
 
+// drivetrain params
+const int pwmStop = 1500;
+const int pwmMin = 1000;
+const int pwmMax = 2000;
+
+// rcl definitions
 rcl_subscription_t subscriber;
 std_msgs__msg__Int16MultiArray msg;
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
-
-
-void motor_update(const std_msgs::Int16MultiArray& joystick_arr) {
-  const std_msgs__msg__Int16MultiArray * motor_msg = (const std_msgs__msg__Int16MultiArray *)msgin;
-  int dtLeftPwm = motor_msg->data.data[0] * 5 + 1500;
-  int dtRightPwm = motor_msg->data.data[1] * 5 + 1500;
-  //TODO: Increment drivetrain motors here (slowly increase or decrease speed)
-  dtLeft[0].writeMicroseconds(dtLeftPwm);
-  dtLeft[1].writeMicroseconds(dtLeftPwm);
-  dtRight[0].writeMicroseconds(dtRightPwm);
-  dtRight[1].writeMicroseconds(dtRightPwm);
-  roboclaw2.ForwardBackwardM1(ROBOCLAW_ADDRESS, motor_msg->data.data[2]); // Excavation linear actuator
-  roboclaw1.ForwardBackwardM1(ROBOCLAW_ADDRESS, motor_msg->data.data[3]); // Excavation drive
-  roboclaw1.ForwardBackwardM2(ROBOCLAW_ADDRESS, motor_msg->data.data[4]); // Hopper linear actuator
 }
 
 
+// MOTOR UPDATE CALLBACK FUNCTION
+void motor_update(const std_msgs::Int16MultiArray& joystick_arr) {
+  const std_msgs__msg__Int16MultiArray * motor_msg = (const std_msgs__msg__Int16MultiArray *)msgin;
+
+  // drivetrain motor speed augmentation to produce a value from 1000 to 2000
+  int dtLeftPwm = motor_msg->data.data[0] * 5 + pwmStop; // expects data[0] to be in range of -100 to 100
+  int dtRightPwm = motor_msg->data.data[1] * 5 + pwmStop; // expects data[1] to be in range of -100 to 100
+
+  // constrain speeds to between 1000 and 2000 (min and max pwm values for drivetrain motor speeds)
+  dtLeftPwm = constrain(dtLeftPwm, pwmMin, pwmMax);
+  dtRightPwm = constrain(dtRightPwm, pwmMin, pwmMax);
+
+  // TODO: Increment drivetrain motors here (slowly increase or decrease speed)
+
+  // drivetrain motors
+  dtLeft.writeMicroseconds(dtLeftPwm);
+  dtRight.writeMicroseconds(dtRightPwm);
+
+  // excavation and hopper motors
+  roboclaw1.ForwardBackwardM2(ROBOCLAW_ADDRESS, motor_msg->data.data[2]); // Excavation drive
+  roboclaw2.ForwardBackwardM1(ROBOCLAW_ADDRESS, motor_msg->data.data[3]); // Excavation linear actuator
+  roboclaw1.ForwardBackwardM1(ROBOCLAW_ADDRESS, motor_msg->data.data[4]); // Hopper linear actuator
+  
+  // possibilty of button debouncing screwing this up. In that case, increment position value to target position (with constraints)
+  // ensure hopper servo is in standard mode, not rotational mode
+  hopperServo.write(motor_msg->data.data[5]); // Hopper servo
+}
+
+// SETUP FUNCTION
 void setup() {
   //TODO: This line may not be needed
   Serial.begin(9600);
 
-  roboclaw1.begin(38400);
-  roboclaw2.begin(38400);
+  // setup roboclaws (excavation lin actuator and motor, hopper lin acuator)
+  roboclaw2.begin(ROBOCLAW_BAUD);
+  roboclaw1.begin(ROBOCLAW_BAUD);
 
-  dtRight[0].attach(14);
-  dtRight[1].attach(15);
-  dtLeft[0].attach(16);
-  dtLeft[1].attach(17);
+  // setup drivetrain motors
+  dtLeft.attach(dtLeftPin);
+  dtRight.attach(dtRightPin);
+
+  // setup hopper servo
+  hopperServo.attach(hopperServoPin);
 
   // Make sure the motors aren't moving
-  roboclaw1.ForwardBackwardM1(ROBOCLAW_ADDRESS, 64);
-  roboclaw1.ForwardBackwardM2(ROBOCLAW_ADDRESS, 64);
-  dtRight[0].writeMicroseconds(1500);
-  dtRight[1].writeMicroseconds(1500);
-  dtLeft[0].writeMicroseconds(1500);
-  dtLeft[1].writeMicroseconds(1500);
+  hopperServo.write(0);
+  roboclaw1.ForwardBackwardM2(ROBOCLAW_ADDRESS, 64); // excavation drive
+  roboclaw1.ForwardBackwardM1(ROBOCLAW_ADDRESS, 64); // hopper linear actuator
+  dtLeft.writeMicroseconds(pwmStop);
+  dtRight.writeMicroseconds(pwmStop);
 
+  // Initialize data array (receiving data from motor_speeds topic)
   msg.data.data = (int16_t *) malloc(sizeof(int16_t) * 10); // Reserve space for 10 int16_t values
   msg.data.size = 0;
   msg.data.capacity = 10;
