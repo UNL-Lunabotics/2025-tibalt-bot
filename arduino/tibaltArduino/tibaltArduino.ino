a9ea5fc6ad1a17e5cf76987fe8109c1a2545f3cc
lunaboticsprogramming <lunaboticsprogramming@protonmail.com>
2025-05-11 13:52:22 -0500

    Added cams to launch py and updated arduino code

--- arduino/tibaltArduino/tibaltArduino.ino
+++ arduino/tibaltArduino/tibaltArduino.ino
@@ -8,14 +8,20 @@
 #include <Servo.h>
 
 #define ROBOCLAW_ADDRESS 0x80
+#define ROBOCLAW_BAUD 38400
 
-RoboClaw roboclaw1(&Serial1, 10000); // M1: excavation drive M2: hopper linear actuator
-RoboClaw roboclaw2(&Serial2, 10000);
+RoboClaw roboclaw1(&Serial1, 10000); // M1: hopper actuator M2: excavation drive
+RoboClaw roboclaw2(&Serial2, 10000); // M1: excavation actuator
 
-Servo dtRight[2];
-Servo dtLeft[2];
-
+Servo dtRight;
+Servo dtLeft;
 Servo hopperLatch;
+
+int dtRightPin = 4;
+int dtLeftPin = 5;
+int hopperServoPin = 9;
+
+const int pwmStop = 1500;
 
 rcl_subscription_t subscriber;
 std_msgs__msg__Int16MultiArray msg;
@@ -25,18 +31,18 @@ rcl_allocator_t allocator;
 rcl_node_t node;
 
 
+//TODO: check topic indices
 void motor_update(const std_msgs::Int16MultiArray& joystick_arr) {
   const std_msgs__msg__Int16MultiArray * motor_msg = (const std_msgs__msg__Int16MultiArray *)msgin;
-  int dtLeftPwm = motor_msg->data.data[0] * 5 + 1500;
-  int dtRightPwm = motor_msg->data.data[1] * 5 + 1500;
+  int dtLeftPwm = motor_msg->data.data[0] * 5 + pwmStop;
+  int dtRightPwm = motor_msg->data.data[1] * 5 + pwmStop;
   //TODO: Increment drivetrain motors here (slowly increase or decrease speed)
-  dtLeft[0].writeMicroseconds(dtLeftPwm);
-  dtLeft[1].writeMicroseconds(dtLeftPwm);
-  dtRight[0].writeMicroseconds(dtRightPwm);
-  dtRight[1].writeMicroseconds(dtRightPwm);
-  roboclaw2.ForwardBackwardM1(ROBOCLAW_ADDRESS, motor_msg->data.data[2]); // Excavation linear actuator
-  roboclaw1.ForwardBackwardM1(ROBOCLAW_ADDRESS, motor_msg->data.data[3]); // Excavation drive
-  roboclaw1.ForwardBackwardM2(ROBOCLAW_ADDRESS, motor_msg->data.data[4]); // Hopper linear actuator
+  dtLeft.writeMicroseconds(dtLeftPwm);
+  dtRight.writeMicroseconds(dtRightPwm);
+  roboclaw1.ForwardBackwardM2(ROBOCLAW_ADDRESS, motor_msg->data.data[2]); // Excavation drive
+  roboclaw2.ForwardBackwardM1(ROBOCLAW_ADDRESS, motor_msg->data.data[3]); // Excavation actuator
+  roboclaw1.ForwardBackwardM1(ROBOCLAW_ADDRESS, motor_msg->data.data[4]); // Hopper actuator
+  hopperServo.write(motor_msg->data.data[5]);
 }
 
 
@@ -44,21 +50,20 @@ void setup() {
   //TODO: This line may not be needed
   Serial.begin(9600);
 
-  roboclaw1.begin(38400);
-  roboclaw2.begin(38400);
+  roboclaw1.begin(ROBOCLAW_BAUD);
+  roboclaw2.begin(ROBOCLAW_BAUD);
 
-  dtRight[0].attach(14);
-  dtRight[1].attach(15);
-  dtLeft[0].attach(16);
-  dtLeft[1].attach(17);
+  dtRight.attach(dtRightPin);
+  dtLeft.attach(dtLeftPin);
+
+  hopperServo.attach(hopperServoPin);
 
   // Make sure the motors aren't moving
   roboclaw1.ForwardBackwardM1(ROBOCLAW_ADDRESS, 64);
   roboclaw1.ForwardBackwardM2(ROBOCLAW_ADDRESS, 64);
-  dtRight[0].writeMicroseconds(1500);
-  dtRight[1].writeMicroseconds(1500);
-  dtLeft[0].writeMicroseconds(1500);
-  dtLeft[1].writeMicroseconds(1500);
+  roboclaw2.ForwardBackwardM1(ROBOCLAW_ADDRESS, 64);
+  dtRight.writeMicroseconds(pwmStop);
+  dtLeft.writeMicroseconds(pwmStop);
 
   msg.data.data = (int16_t *) malloc(sizeof(int16_t) * 10); // Reserve space for 10 int16_t values
   msg.data.size = 0;
