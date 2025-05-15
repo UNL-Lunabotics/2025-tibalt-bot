2855246a2600c17c02ec2f85fc972ea0e9d34323
lunaboticsprogramming <lunaboticsprogramming@protonmail.com>
2025-05-15 11:29:06 -0500

    fixed issue with realsense wrapper and realsense sdk having conflicting vesions. also fixed timestamp issues (it is not 1969 anymore)

---
+++ build/realsense2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lunabotics/2025-tibalt-bot/src/realsense2_camera/realsense2_description
+# Build directory: /home/lunabotics/2025-tibalt-bot/build/realsense2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- build/rover_control/share/rover_control/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/lunabotics/2025-tibalt-bot/build/rover_control"
