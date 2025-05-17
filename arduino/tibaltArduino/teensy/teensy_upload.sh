c1e97c8450bd10e212296e8b99d7788d44ba2053
lunaboticsprogramming <lunaboticsprogramming@protonmail.com>
2025-05-17 17:36:44 -0400

    pkvgn

--- arduino/tibaltArduino/teensy/teensy_upload.sh
+++ arduino/tibaltArduino/teensy/teensy_upload.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 # Path to your compiled firmware
+CHIP="gpiochip0"
+LINE_OFFSET=79  # GPIO27 = Pin 13 on Jetson Orin Nano
 HEX_PATH=".pio/build/teensy41/firmware.hex"
-
 sleep 0.5
+
+echo "[INFO] Triggering Teensy bootloader on $CHIP line $LINE_OFFSET..."
+
+# Trigger pin LOW then HIGH via gpiod
+python3 - <<END
+import gpiod
+import time
+
+chip = gpiod.Chip("$CHIP")
+line = chip.get_line($LINE_OFFSET)
+line.request(consumer="teensy-reset", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[1])
+line.set_value(0)
+time.sleep(0.5)
+line.set_value(1)
+line.release()
+print("[INFO] Bootloader triggered.")
+END
+
+sleep 1
 
 # Upload using teensy_loader_cli (must be installed and in PATH)
 # -w: wait for device
